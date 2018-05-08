8e182b0e412ab77e132a1b53b3738cbff6acaaaa
Mo Mesgin <mo.mesgin@gmail.com>
2018-05-08 13:51:17 -0700

    profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const Profile = require('../../models/Profile')
+const passport = require('passport')
 
-router.get('/',(req,res)=>{
-  res.json({msg:'profile'})
+router.get('/', passport.authenticate('jwt',{session:false}), (req,res)=>{
+  const errors = {}
+  Profile.findOne({user:req.user.id})
+    .then(profile => {
+      if(!profile){
+        errors.noProfile = 'No profile found'
+        res.status(404).json(errors)
+      }
+      res.json(profile)
+    })
 })
 
 module.exports = router
